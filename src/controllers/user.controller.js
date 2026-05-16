0411beb03b50b68268482c285938c4f0df2a1a51
Harsh Sharma <harshsharmahs.081@gmail.com>
2026-05-16 14:26:46 +0530

    changes in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -73,7 +73,7 @@ const registerUser = asyncHandler( async(req,res)=>{
     }
 
     return res.status(201).json(
-        new ApiResponse(200,"User registered successfully!")
+        new ApiResponse(200,createdUser,"User registered successfully!")
     )
 
 } )
