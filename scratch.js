c501cf9e2a2716214c92425554356b90260d4ebd
Harsh Sharma <harshsharmahs.081@gmail.com>
2026-05-16 14:58:05 +0530

    remove temporary test scripts and sample image assets

--- scratch.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' });
-import fs from 'fs';
-import { uploadOnCloudinary } from './src/utils/cloudinary.js';
-
-fs.writeFileSync('test.jpg', 'test content');
-uploadOnCloudinary('test.jpg').then(res => console.log(res));
